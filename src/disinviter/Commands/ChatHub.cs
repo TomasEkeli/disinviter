7ded4a2d74f332b5ec6bfad0d2800efcade8ea76
Tomas Ekeli <tomas@eke.li>
2022-07-25 20:47:04 +0000

    add some fun sounds (one not added to git for copyright reasons)

--- src/disinviter/Commands/ChatHub.cs
+++ src/disinviter/Commands/ChatHub.cs
@@ -9,6 +9,7 @@ namespace disinviter.Commands;
 public class ChatHub : Hub
 {
     const string EventSourceId = "ChatHub";
+    readonly Random _random = new();
     readonly IDolittleClient _dolittleClient;
 
     public ChatHub(IDolittleClient dolittleClient)
@@ -33,6 +34,75 @@ public class ChatHub : Hub
                 DateTimeOffset.UtcNow,
                 user,
                 message
+            );
+    }
+
+    public async Task Play(string user, string soundName)
+    {
+        await _dolittleClient
+            .EventStore
+            .ForTenant(SingleTenant.TenantId)
+            .CommitEvent(
+                new SoundPlayed(user, soundName),
+                EventSourceId
+            );
+
+        await Clients
+            .All
+            .SendAsync(
+                "SoundPlayed",
+                user,
+                soundName
+            );
+    }
+
+    public async Task Toast(string user)
+    {
+        await _dolittleClient
+            .EventStore
+            .ForTenant(SingleTenant.TenantId)
+            .CommitEvent(
+                new Toast(user),
+                EventSourceId
+            );
+
+        if (_random.Next(2) == 0)
+        {
+            await Clients
+                .All
+                .SendAsync(
+                    "SoundPlayed",
+                    user,
+                    "tore"
+                );
+        }
+        else
+        {
+            await Clients
+                .All
+                .SendAsync(
+                    "SoundPlayed",
+                    user,
+                    "mike"
+                );
+        }
+    }
+
+    public async Task Stop(string user)
+    {
+        await _dolittleClient
+            .EventStore
+            .ForTenant(SingleTenant.TenantId)
+            .CommitEvent(
+                new SoundStopped(user),
+                EventSourceId
+            );
+
+        await Clients
+            .All
+            .SendAsync(
+                "SoundStopped",
+                user
             );
     }
 
--- src/disinviter/Events/ChatMessageSent.cs
+++ src/disinviter/Events/ChatMessageSent.cs
@@ -4,3 +4,12 @@ namespace disinviter.Events;
 
 [EventType("B0B579B0-2A01-40AC-8247-4D5A2706DBD1")]
 public record ChatMessageSent(string User, string Message);
+
+[EventType("4033D2B2-8039-4EE1-A4E5-79AE9B115388")]
+public record SoundPlayed(string User, string SoundName);
+
+[EventType("BD99A245-2767-4D9E-A756-3D04C504B6C0")]
+public record SoundStopped(string User);
+
+[EventType("178A8F0F-EFC0-4CB7-830B-3EAB7D8D6B1C")]
+public record Toast(string User);
